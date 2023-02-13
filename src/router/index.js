82ff8890cfbcbb74c56da33c8e5a01ba811fc60a
Sonny Luu <sonnyluu@gmail.com>
2023-02-13 22:50:57 +0100

    vue router install and config

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "/src/views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "Sonny Luu",
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;

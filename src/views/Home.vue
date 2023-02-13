eab6215993e6018bf4a3b3fb5b048c91c7633ba1
Sonny Luu <sonnyluu@gmail.com>
2023-02-13 22:55:39 +0100

    component test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,10 @@
-<template>hello</template>
+<template>
+  <Header />
+  <div>hello, my name is {{ $route.meta.title }}</div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import Header from "../components/Header.vue";
+const router = useRouter();
+</script>

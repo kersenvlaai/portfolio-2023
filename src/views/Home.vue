b29fbed37c0b42ac087c844bd0de1a30febc6325
Sonny Luu <sonnyluu@gmail.com>
2023-02-14 19:24:06 +0100

    install tw elements and add experience tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
   <div>hello, my name is {{ $route.meta.title }}</div>
+  <Experience />
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
 import Header from "../components/Header.vue";
+import Experience from "../components/Experience.vue";
 const router = useRouter();
 </script>

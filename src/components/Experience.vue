b9f6ad9da1088cc8bd45f80925e334ff00f22556
Sonny Luu <sonnyluu@gmail.com>
2023-02-19 21:02:45 +0100

    add prettier file and added 4th tab

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,79 +1,101 @@
 <template>
-  <div class="flex items-start">
-    <ul
-      class="nav nav-tabs flex flex-col flex-wrap list-none border-b-0 pl-0 mr-4"
-      id="tabs-tabVertical"
-      role="tablist"
-    >
-      <li class="nav-item flex-grow text-center" role="presentation">
-        <a
-          href="#tabs-homeVertical"
-          class="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-l-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
-          id="tabs-home-tabVertical"
-          data-bs-toggle="pill"
-          data-bs-target="#tabs-homeVertical"
-          role="tab"
-          aria-controls="tabs-homeVertical"
-          aria-selected="true"
-          >Adyen</a
+    <h2>Experience</h2>
+    <div class="flex items-start">
+        <ul
+            class="nav nav-tabs flex flex-col flex-wrap list-none border-b-0 pl-0 mr-4"
+            id="tabs-tabVertical"
+            role="tablist"
         >
-      </li>
-      <li class="nav-item flex-grow text-center" role="presentation">
-        <a
-          href="#tabs-profileVertical"
-          class="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
-          id="tabs-profile-tabVertical"
-          data-bs-toggle="pill"
-          data-bs-target="#tabs-profileVertical"
-          role="tab"
-          aria-controls="tabs-profileVertical"
-          aria-selected="false"
-          >Capgemini</a
-        >
-      </li>
-      <li class="nav-item flex-grow text-center" role="presentation">
-        <a
-          href="#tabs-messagesVertical"
-          class="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
-          id="tabs-messages-tabVertical"
-          data-bs-toggle="pill"
-          data-bs-target="#tabs-messagesVertical"
-          role="tab"
-          aria-controls="tabs-messagesVertical"
-          aria-selected="false"
-          >Atos</a
-        >
-      </li>
-    </ul>
-    <div class="tab-content" id="tabs-tabContentVertical">
-      <div
-        class="tab-pane fade show active"
-        id="tabs-homeVertical"
-        role="tabpanel"
-        aria-labelledby="tabs-home-tabVertical"
-      >
-        Tab 1 content vertical
-      </div>
-      <div
-        class="tab-pane fade"
-        id="tabs-profileVertical"
-        role="tabpanel"
-        aria-labelledby="tabs-profile-tabVertical"
-      >
-        Tab 2 content vertical
-      </div>
-      <div
-        class="tab-pane fade"
-        id="tabs-messagesVertical"
-        role="tabpanel"
-        aria-labelledby="tabs-profile-tabVertical"
-      >
-        Tab 3 content vertical
-      </div>
+            <li class="nav-item flex-grow text-center" role="presentation">
+                <a
+                    href="#tabs-homeVertical"
+                    class="nav-link block font-medium text-xs leading-tight border-x-0 border-t-0 border-l-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
+                    id="tabs-home-tabVertical"
+                    data-bs-toggle="pill"
+                    data-bs-target="#tabs-homeVertical"
+                    role="tab"
+                    aria-controls="tabs-homeVertical"
+                    aria-selected="true"
+                    >Adyen</a
+                >
+            </li>
+            <li class="nav-item flex-grow text-center" role="presentation">
+                <a
+                    href="#tabs-profileVertical"
+                    class="nav-link block font-medium text-xs leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
+                    id="tabs-profile-tabVertical"
+                    data-bs-toggle="pill"
+                    data-bs-target="#tabs-profileVertical"
+                    role="tab"
+                    aria-controls="tabs-profileVertical"
+                    aria-selected="false"
+                    >Capgemini</a
+                >
+            </li>
+            <li class="nav-item flex-grow text-center" role="presentation">
+                <a
+                    href="#tabs-messagesVertical"
+                    class="nav-link block font-medium text-xs leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
+                    id="tabs-messages-tabVertical"
+                    data-bs-toggle="pill"
+                    data-bs-target="#tabs-messagesVertical"
+                    role="tab"
+                    aria-controls="tabs-messagesVertical"
+                    aria-selected="false"
+                    >Atos</a
+                >
+            </li>
+            <li class="nav-item flex-grow text-center" role="presentation">
+                <a
+                    href="#tabs-ICT-GroupVertical"
+                    class="nav-link block font-medium text-xs leading-tight border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
+                    id="tabs-ICTGroup-tabVertical"
+                    data-bs-toggle="pill"
+                    data-bs-target="#tabs-ICT-GroupVertical"
+                    role="tab"
+                    aria-controls="tabs-ICT-GroupVertical"
+                    aria-selected="false"
+                    >ICT Group</a
+                >
+            </li>
+        </ul>
+        <div class="tab-content" id="tabs-tabContentVertical">
+            <div
+                class="tab-pane fade show active"
+                id="tabs-homeVertical"
+                role="tabpanel"
+                aria-labelledby="tabs-home-tabVertical"
+            >
+                Tab 1 content vertical
+            </div>
+            <div
+                class="tab-pane fade"
+                id="tabs-profileVertical"
+                role="tabpanel"
+                aria-labelledby="tabs-profile-tabVertical"
+            >
+                Tab 2 content vertical
+            </div>
+            <div
+                class="tab-pane fade"
+                id="tabs-messagesVertical"
+                role="tabpanel"
+                aria-labelledby="tabs-profile-tabVertical"
+            >
+                Tab 3 content vertical
+            </div>
+            <div
+                class="tab-pane fade"
+                id="tabs-ICT-GroupVertical"
+                role="tabpanel"
+                aria-labelledby="tabs-profile-tabVertical"
+            >
+                Tab 4 content vertical
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import "tw-elements";
+import 'tw-elements';
 </script>
